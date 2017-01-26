0ad4228cb3c4af636d22d4e6f89f89d7239961c5
Richard Gowers <richardjgowers@gmail.com>
2017-01-26 16:56:55 +0000

    Added status checking to check

--- paraspa/check.py
+++ paraspa/check.py
@@ -1,19 +1,44 @@
 from __future__ import print_function
 
 import glob
+import os
 
+STATUSES = {
+    0: 'FINISHED',
+    1: 'IN PROGRESS',
+    2: 'NOT STARTED',
+}
 
 def check(dirname):
     """Check the progress of tasks starting with *dirname*"""
     children = glob.glob('{}_part_*'.format(dirname))
+    # order children
+    def childno(name):
+        return int(name.split('_')[-1])
+    children = sorted(children, key=childno)
 
     alldone = True
 
     print("Found {} children".format(len(children)))
     for child in children:
-        finished = True
-        print(" - {} is {}".format(child, 'FINISHED'))
-        alldone = alldone and finished
+        status = is_finished(child)
+        print(" - {} is {}".format(child, STATUSES[status]))
+        alldone = alldone and (status == 0)
     if alldone:
         print("All simulations finished!")
 
+
+def is_finished(dirname):
+    """Check if raspa simulation in *dirname* finished"""
+    # fn of output file
+    try:
+        output = glob.glob(os.path.join(dirname, 'Output', 'System_0', '*.data'))[0]
+    except IndexError:  # output not created, sim not started
+        return 2
+    with open(output, 'r') as f:
+        f.seek(-100, 2)  # seek to 100 bytes before EOF
+        done = 'Simulation finished' in f.read()
+    if done:
+        return 0
+    else:
+        return 1
--- tests/test_check.py
+++ tests/test_check.py
@@ -14,11 +14,20 @@ class TestCheck(object):
 
         assert "Found 3 children" in out
 
-    def test_finds_finished(self):
-        pass
+    def test_finds_finished(self, capsys):
+        prsp.check('chk')
+        out, err = capsys.readouterr()
 
-    def test_finds_incomplete(self):
-        pass
+        assert ' - chk_part_1 is FINISHED' in out.split('\n')
 
-    def test_reports_all_finished(self):
-        pass
+    def test_finds_in_progress(self, capsys):
+        prsp.check('chk')
+        out, err = capsys.readouterr()
+
+        assert ' - chk_part_3 is IN PROGRESS' in out.split('\n')
+
+    def test_finds_incomplete(self, capsys):
+        prsp.check('chk')
+        out, err = capsys.readouterr()
+
+        assert ' - chk_part_2 is NOT STARTED' in out.split('\n')

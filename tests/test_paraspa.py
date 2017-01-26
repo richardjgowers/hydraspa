f081bee25f31bca672972549652e3ed5330efa99
Richard Gowers <richardjgowers@gmail.com>
2017-01-26 12:33:16 +0000

    TST: added tests for split

--- tests/test_paraspa.py
+++ tests/test_paraspa.py
@@ -1,1 +1,20 @@
-#
+import os
+
+import paraspa as prsp
+
+class TestSplit(object):
+    FILES = ['file1.txt', 'file2.txt']
+
+    def test_split_dirs(self):
+        prsp.split('mysim', ntasks=2)
+
+        # check that required directories were made
+        for i in ['1', '2']:
+            assert os.path.exists('mysim_part_{}'.format(i))
+
+    def test_split_files(self):
+        prsp.split('mysim', ntasks=2)
+
+        for d in ('mysim_part_1', 'mysim_part_2'):
+            for fn in self.FILES:
+                assert os.path.exists(os.path.join(d, fn))

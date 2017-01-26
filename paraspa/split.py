cf5fd84b81c3c87bcd2c42afca454f696bf50de1
Richard Gowers <richardjgowers@gmail.com>
2017-01-26 14:07:00 +0000

    Split now duplicates directories!

--- paraspa/split.py
+++ paraspa/split.py
@@ -1,3 +1,24 @@
+"""Splits a single simulation dir into many
+
+"""
+import os
+import shutil
+
+
 def split(src, ntasks):
-    """Split simulation in *src* into *ntasks*"""
-    pass
+    """Split simulation in *src* into *ntasks*
+
+    Parameters
+    ----------
+    src : string
+      Name of the directory we wish to copy
+    ntasks : int
+      Number of copies of *src* to make
+    """
+    newdirs = []
+
+    for i in range(ntasks):
+        newname = '{}_part_{}'.format(src, i+1)
+        newdirs.append(newname)
+
+        shutil.copytree(src, newname)
--- tests/test_split.py
+++ tests/test_split.py
@@ -1,19 +1,33 @@
+import glob
 import os
+import shutil
+import pytest
 
 import paraspa as prsp
+
+@pytest.fixture
+def rm_dirs():
+    yield
+    for d in glob.glob('*part_*'):
+        try:
+            shutil.rmtree(d)
+        except OSError:
+            pass
 
 class TestSplit(object):
     FILES = ['file1.txt', 'file2.txt']
 
-    def test_split_dirs(self):
+    def test_split_dirs(self, rm_dirs):
         prsp.split('mysim', ntasks=2)
 
         # check that required directories were made
         for i in ['1', '2']:
             assert os.path.exists('mysim_part_{}'.format(i))
 
-    def test_split_files(self):
+    def test_split_files(self, rm_dirs):
         prsp.split('mysim', ntasks=2)
+
+        # check that each directory made had all the required files
 
         for d in ('mysim_part_1', 'mysim_part_2'):
             for fn in self.FILES:

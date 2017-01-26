07e7089f912129de47fc4f4e1a9a257ef5493f1e
Richard Gowers <richardjgowers@gmail.com>
2017-01-26 15:25:13 +0000

    TST: Moved rm_dirs into conftest

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import glob
+import os
+import pytest
+import shutil
+
+
+@pytest.fixture(scope='function')
+def rm_dirs():
+    yield
+    for d in glob.glob('*part_*'):
+        try:
+            shutil.rmtree(d)
+        except OSError:
+            pass
+    try:
+        os.remove('qsub_mysim.sh')
+    except OSError:
+        pass
--- tests/test_split.py
+++ tests/test_split.py
@@ -5,18 +5,6 @@ import pytest
 
 import paraspa as prsp
 
-@pytest.fixture
-def rm_dirs():
-    yield
-    for d in glob.glob('*part_*'):
-        try:
-            shutil.rmtree(d)
-        except OSError:
-            pass
-    try:
-        os.remove('qsub_mysim.sh')
-    except OSError:
-        pass
 
 def runlength(d):
     with open(os.path.join(d, 'simulation.input'), 'r') as f:
@@ -37,11 +25,12 @@ qsub qsub.sh
 cd ../
 """
 
+@pytest.mark.usefixtures('rm_dirs')
 class TestSplit(object):
     FILES = ['file1.txt', 'file2.txt', 'simulation.input']
 
     @pytest.mark.parametrize('ntasks', [1, 2, 3, 4, 5])
-    def test_split_dirs(self, ntasks, rm_dirs):
+    def test_split_dirs(self, ntasks):
         prsp.split('mysim', ntasks=ntasks)
 
         # check that required directories were made
@@ -49,7 +38,7 @@ class TestSplit(object):
             assert os.path.exists('mysim_part_{}'.format(i + 1))
         assert not os.path.exists('mysim_part_{}'.format(i + 2))
 
-    def test_split_files(self, rm_dirs):
+    def test_split_files(self):
         prsp.split('mysim', ntasks=2)
 
         # check that each directory made had all the required files
@@ -63,18 +52,18 @@ class TestSplit(object):
         (3, 333334),
         (4, 250001),
     ])
-    def test_check_runlength(self, ntasks, expected, rm_dirs):
+    def test_check_runlength(self, ntasks, expected):
         prsp.split('mysim', ntasks=ntasks)
 
         for d in ('mysim_part_1', 'mysim_part_2'):
             assert runlength(d) == expected
 
-    def test_check_qsubber_made(self, rm_dirs):
+    def test_check_qsubber_made(self):
         prsp.split('mysim', ntasks=2)
 
         assert os.path.exists('qsub_mysim.sh')
 
-    def test_check_qsubber_contents(self, rm_dirs):
+    def test_check_qsubber_contents(self):
         prsp.split('mysim', ntasks=2)
 
         with open('qsub_mysim.sh', 'r') as f:

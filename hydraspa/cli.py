ee0f544322224b7fd3550441788f1f5a64b60b87
Richard Gowers <richardjgowers@gmail.com>
2017-02-06 19:01:32 +0000

    Fixed version cli not working

--- hydraspa/cli.py
+++ hydraspa/cli.py
@@ -4,10 +4,11 @@ Usage:
   hydraspa split <dirname> [-n <N> -c <C> (-P <P>...)]
   hydraspa check <dirname>
   hydraspa gather <dirname>
+  hydraspa --version
 
 Options:
   -h --help
-  --version
+  --version                       Show the version
   -n NTASKS, --ntasks NTASKS      Number of duplicates per pressure point [default: 1]
   -c NCYCLES, --ncycles NCYCLES   Number of cycles to perform *each* sim for
   -P, --pressures                 Define list of pressures in kPa

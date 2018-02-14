440c0e6ca80ba01a250d4134aa3d815c2b6aaa2d
Richard Gowers <richardjgowers@gmail.com>
2018-02-14 16:50:25 +0000

    removed unfinished test

--- tests/test_gather.py
+++ tests/test_gather.py
@@ -7,6 +7,7 @@ import hydraspa as hrsp
 SERIES1 = [0.0, 1.5, 3.5, 10.0]
 SERIES2 = [0.0, 2.2, 4.4, 6.6]
 
+
 @pytest.fixture
 def gather_results():
     return hrsp.gather.gather('gth')
@@ -17,18 +18,11 @@ class TestGather(object):
         assert isinstance(gather_results, dict)
         for thing in gather_results.values():
             assert isinstance(thing, np.ndarray)
-    
+
     def test_found_dirs(self, gather_results):
         assert 'gth_1234567_part1' in gather_results
         assert 'gth_1234567_part2' in gather_results
-        
+
     def test_parsed_values(self, gather_results):
         assert all(gather_results['gth_1234567_part1'] == SERIES1)
         assert all(gather_results['gth_1234567_part2'] == SERIES2)
-
-
-
-@pytest.fixture
-def gather_dir():
-    with mock.patch('hydraspa.gather.parse_output') as po:
-        pass
